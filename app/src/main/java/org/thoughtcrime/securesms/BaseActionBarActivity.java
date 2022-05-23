a55ac5fa15a0fa3c7c119710494dd284f060f1a3
Md Shakawat Hossain <76183053+shakawat21@users.noreply.github.com>
2022-05-23 11:54:53 +0600

    ...........

--- app/src/main/java/org/thoughtcrime/securesms/BaseActionBarActivity.java
+++ app/src/main/java/org/thoughtcrime/securesms/BaseActionBarActivity.java
@@ -17,48 +17,48 @@ import org.session.libsession.utilities.dynamiclanguage.DynamicLanguageContextWr
 import network.loki.messenger.R;
 
 public abstract class BaseActionBarActivity extends AppCompatActivity {
-  private static final String TAG = BaseActionBarActivity.class.getSimpleName();
+    private static final String TAG = BaseActionBarActivity.class.getSimpleName();
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    ActionBar actionBar = getSupportActionBar();
-    if (actionBar != null) {
-      actionBar.setDisplayHomeAsUpEnabled(true);
-      actionBar.setHomeButtonEnabled(true);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        super.onCreate(savedInstanceState);
     }
 
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    initializeScreenshotSecurity();
-    DynamicLanguageActivityHelper.recreateIfNotInCorrectLanguage(this, TextSecurePreferences.getLanguage(this));
-    String name = getResources().getString(R.string.app_name);
-    Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
-    int color = getResources().getColor(R.color.app_icon_background);
-    setTaskDescription(new ActivityManager.TaskDescription(name, icon, color));
-  }
-
-  @Override
-  public boolean onSupportNavigateUp() {
-    if (super.onSupportNavigateUp()) return true;
-
-    onBackPressed();
-    return true;
-  }
-
-  private void initializeScreenshotSecurity() {
-    if (TextSecurePreferences.isScreenSecurityEnabled(this)) {
-      getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-    } else {
-      getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializeScreenshotSecurity();
+        DynamicLanguageActivityHelper.recreateIfNotInCorrectLanguage(this, TextSecurePreferences.getLanguage(this));
+        String name = getResources().getString(R.string.app_name);
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
+        int color = getResources().getColor(R.color.app_icon_background);
+        setTaskDescription(new ActivityManager.TaskDescription(name, icon, color));
     }
-  }
 
-  @Override
-  protected void attachBaseContext(Context newBase) {
-    super.attachBaseContext(DynamicLanguageContextWrapper.updateContext(newBase, TextSecurePreferences.getLanguage(newBase)));
-  }
+    @Override
+    public boolean onSupportNavigateUp() {
+        if (super.onSupportNavigateUp()) return true;
+
+        onBackPressed();
+        return true;
+    }
+
+    private void initializeScreenshotSecurity() {
+        if (TextSecurePreferences.isScreenSecurityEnabled(this)) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+        } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+        }
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(DynamicLanguageContextWrapper.updateContext(newBase, TextSecurePreferences.getLanguage(newBase)));
+    }
 }
